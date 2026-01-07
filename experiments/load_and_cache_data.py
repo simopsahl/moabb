2a06252e3e7c845f09dc77b6bd203398dd1e6195
simopsahl <sopsahl@numenta.com>
2026-01-06 18:51:17 -0800

    Changed to load all data

--- experiments/load_and_cache_data.py
+++ experiments/load_and_cache_data.py
@@ -1,15 +1,14 @@
 import moabb
+from moabb import set_log_level
+set_log_level("info")
+from tqdm import tqdm
 
 def load_and_cache_data(datasets: list[moabb.datasets.base.BaseDataset], path: str):
-    from moabb import set_log_level
-    set_log_level("info")
-    from tqdm import tqdm
     for dataset in tqdm(datasets, desc="Loading datasets"):
         dataset.get_data(cache_config=dict(use=True, path=path, save_raw=True))
 
 # This script is used to load and cache the data for the experiments.
-
 if __name__ == "__main__":
     OUTPUT_PATH = "/mnt/results/sopsahl/moabb/data/raw"
-    datasets = [moabb.datasets.Zhou2016(), moabb.datasets.alex_mi.AlexMI()]
+    datasets = [dataset() for dataset in moabb.datasets.utils.dataset_search()]
     load_and_cache_data(datasets, OUTPUT_PATH)
